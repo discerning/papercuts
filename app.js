7ac04a5a6de4c531d9a65ef44cae3d98d1404c07
Giordon Stark <kratsg@gmail.com>
2016-03-24 15:29:15 -0500

    make sure we respond to error messages with json when the request headers are json

--- app.js
+++ app.js
@@ -58,22 +58,34 @@ app.use(function logErrors(err, req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
+    if(req.headers["content-type"] == 'application/json'){
+        res.status(err.status || 500);
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({message: err.message, error: err}));
+    } else {
+        res.status(err.status || 500);
+        res.render('error', {
+          message: err.message,
+          error: err
+        });
+    }
   });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    if(req.headers["content-type"] == 'application/json'){
+        res.status(err.status || 500);
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({message: err.message, error: {}}));
+    } else {
+        res.status(err.status || 500);
+        res.render('error', {
+          message: err.message,
+          error: {}
+        });
+    }
 });
 
 
