ed0c19b172fa3b5561f16195fab615941ac863d1
Giordon Stark <kratsg@gmail.com>
2016-03-25 16:27:58 -0500

    Update by adding a schema.js configuration specifying all schema we match against

---
+++ config/schema.js
@@ -0,0 +1,3 @@
+module.exports = {
+    cutflow: require('../public/js/schema/cutflow.json')
+}
--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var firebase = require('../services/firebase');
+var schema = require('../config/schema');
 
 var json_validator = require('jsonschema').validate;
 
@@ -20,24 +21,24 @@ router.post('/analysis/:analysis', function(req, res, next) {
     firebase.child(analysis).once("value", function(snapshot){
         if(!snapshot.exists()){
             res.status(400);
-            res.send(JSON.stringify({analysis: analysis, error: 'An analysis by that name does not exist!'}));
+            res.send(JSON.stringify({analysis: analysis, message: 'An analysis by that name does not exist!', error: true, errors: []}));
             return;
         }
         // next: ensure that they have the right secret to post shit
         if(snapshot.child('secret').val() != req.query.token){
             res.status(401);
-            res.send(JSON.stringify({analysis: analysis, error: 'Authentication failed. Is the token correct?'}));
+            res.send(JSON.stringify({analysis: analysis, message: 'Authentication failed. Is the token correct?', error: true, errors: []}));
             return;
         }
         // next: check that the json passes the schema
-        var json_check = json_validator(req.body, schema.analysis);
+        var json_check = json_validator(req.body, schema.cutflow);
         if(!json_check.valid){
             res.status(400);
-            res.send(JSON.stringify({analysis: analysis, error: json_check.errors}));
+            res.send(JSON.stringify({analysis: analysis, message: 'JSON is not valid for the cutflow schema.', error: true, errors: json_check.errors}));
             return;
         }
         // i guess everything checks out
-        res.send(JSON.stringify({analysis: analysis, error: null}));
+        res.send(JSON.stringify({analysis: analysis, error: false, errors: []}));
     });
 });
 
