f6b008e29adf42915a2940c34bcd2538415a6bd3
Giordon Stark <kratsg@gmail.com>
2016-03-29 02:02:35 -0500

    make it auth for each firebase instance we want

--- services/firebase.js
+++ services/firebase.js
@@ -4,18 +4,11 @@ var config = require('../config');
 var Firebase = require('firebase');
 var FirebaseTokenGenerator = require('firebase-token-generator');
 
-var ref = new Firebase(config.services.firebase.url);
 var tokenGenerator = new FirebaseTokenGenerator(config.services.firebase.secret);
 
 // data on the inside for authentication
 module.exports = function(data){
-    ref.authWithCustomToken(tokenGenerator.createToken(data), function(err, authData){
-        if(err){
-            console.log("Login Failed!", err);
-            return false;
-        } else {
-            console.log("Login Succeeded!", authData);
-            return ref;
-        }
-    });
+    var ref = new Firebase(config.services.firebase.url);
+    ref.authWithCustomToken(tokenGenerator.createToken(data));
+    return ref;
 };
