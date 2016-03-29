e6a890b697daae798b8a8335599db322c57bc750
Giordon Stark <kratsg@gmail.com>
2016-03-29 12:28:59 -0500

    update to use bearer token

--- services/passport.js
+++ services/passport.js
@@ -1,14 +1,37 @@
 // load the auth variables
 var config = require('../config');
 
+// get user info
+var https = require('https');
+var url = require('url');
+
 var OAuth2Strategy = require('passport-oauth').OAuth2Strategy.Strategy;
 
 module.exports = function(passport) {
 
     var oauth2 = new OAuth2Strategy(config.services.cern,
         function(accessToken, refreshToken, profile, done){
-            console.log('Profile: %j', profile);
-            return done(null, profile);
+            var URI = url.parse(config.services.cern.infoURL);
+            var user_req = https.request({
+                host: URI.hostname,
+                path: URI.pathname,
+                port: URI.port,
+                method: 'GET',
+                headers: {
+                    Authorization: "Bearer "+accessToken
+                }
+            }, function(res){
+                var body = '';
+                res.on('data', function(chunk){ body+= chunk; });
+                res.on('end', function(){
+                    var profile = JSON.parse(body);
+                    console.log('Profile: %j', profile);
+                    return done(null, profile);
+                });
+            }).on('error', function(e){
+                console.log("Got an error: ", e);
+                return done(e);
+            });
         }
     );
 
