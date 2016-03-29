0b08ac71e1f3e1dd0895be28b48d9090cbffbd9b
Giordon Stark <kratsg@gmail.com>
2016-03-29 12:28:31 -0500

    add info url

--- config/services.js
+++ config/services.js
@@ -2,6 +2,7 @@ module.exports = {
     cern: {
         authorizationURL: 'https://oauth.web.cern.ch/OAuth/Authorize',
         tokenURL: 'https://oauth.web.cern.ch/OAuth/Token',
+        infoURL: 'https://oauthresource.web.cern.ch/api/Me',
         clientID: 'papercuts',
         clientSecret: (typeof process.env.OAUTH2_SECRET === 'undefined') ? 'secret' : process.env.OAUTH2_SECRET,
         callbackURL: '/auth/oauth2/callback'
